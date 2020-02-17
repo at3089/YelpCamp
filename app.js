5985b02dca1861ceba1652051a5e30510f40cd52
Anthony Taldone <at3089@nyu.edu>
2020-02-16 22:34:12 -0500

    Add Creating Campgrounds

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+let campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1546890975-7596e98cdbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1514960821766-38f2cac1e2d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
+  }
+];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  let campgrounds = [
-    {
-      name: "Salmon Creek",
-      image:
-        "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
-    },
-    {
-      name: "Granite Hill",
-      image:
-        "https://images.unsplash.com/photo-1546890975-7596e98cdbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-    },
-    {
-      name: "Mountain Goat's Rest",
-      image:
-        "https://images.unsplash.com/photo-1514960821766-38f2cac1e2d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
-    }
-  ];
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+  // get data from form and add to campgrounds array
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  // redirect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
 });
 
 app.listen(port, () =>
