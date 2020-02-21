ee6a22e639ebfe2f09ab64257ae277c168d8c8aa
Anthony Taldone <at3089@nyu.edu>
2020-02-21 10:47:16 -0500

    Add User Login

--- app.js
+++ app.js
@@ -157,6 +157,21 @@ app.post("/register", function(req, res) {
   });
 });
 
+// show login form
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+// handle login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
+
+
 // Listen on Port 3000
 app.listen(port, () =>
   console.log(`The YelpCamp Server Has Started! ... listening on port ${port}!`)
