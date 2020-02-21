281339b622fcda52b268d851d4de9c65b4c81909
Anthony Taldone <at3089@nyu.edu>
2020-02-21 10:13:24 -0500

    Configure Passport

--- app.js
+++ app.js
@@ -23,6 +23,20 @@ app.use(express.static(__dirname + "/public"));
 // const seedDB = require("./seeds");
 // seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(
+  require("express-session")({
+    secret: "The quick brown fox",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res) {
   res.render("landing");
 });
